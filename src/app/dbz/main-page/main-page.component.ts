ed3a31085d989c0c1a56c8ed51343b33b9fbe6cf
karen <karen.acuna@ospinn.com>
2022-01-28 15:56:50 -0500

    ''primeros pasos con modulos, componentes y servicios"

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  // get personajes(): Personaje[] {
+  //   return this.dbzService.personajes;
+  // }
+
+  constructor() {}
+
+}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from "@angular/core";
 import { HeroeComponent } from './heroe/heroe.component';
 import { ListadoComponent } from './listado/listado.component';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { ListadoComponent } from './listado/listado.component';
     exports: [
         ListadoComponent,
     ],
+    imports: [CommonModule]
     
 })
 export class HeroesModule { }
